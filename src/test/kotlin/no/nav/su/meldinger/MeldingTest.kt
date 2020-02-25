942c4cb263c2b51c68df1916db6bc7d383f8482d
Jacob Meidell <jacob.meidell@nav.no>
2020-02-25 15:19:10 +0100

    Add dummy tests.

---
+++ src/test/kotlin/no/nav/su/meldinger/MeldingTest.kt
@@ -0,0 +1,11 @@
+package no.nav.su.meldinger
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class MeldingTest {
+    @Test
+    fun test(){
+        assertTrue(true)
+    }
+}
