1b9de6e1cb3389f78b3f023f625fd27a5142e44f
Jacob Meidell <jacob.meidell@nav.no>
2020-02-25 15:11:10 +0100

    Configured publishing of artifacts from local image. Using git hash for version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,22 @@
+import java.io.ByteArrayOutputStream
+
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.3.61"
+    id("java")
+    id("maven-publish")
 }
 
-version = "0.0.1"
+val gitHash = ByteArrayOutputStream().use {
+    exec {
+        commandLine("git", "rev-parse", "HEAD")
+        standardOutput = it
+    }
+    it.toString().trim()
+}
+
+group = "no.nav"
+version = gitHash
+
 apply(plugin = "org.jetbrains.kotlin.jvm")
 java {
     sourceCompatibility = JavaVersion.VERSION_12
@@ -16,10 +30,44 @@ repositories {
     mavenCentral()
 }
 
+configure<PublishingExtension> {
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.github.com/navikt/su-meldinger")
+            credentials {
+                username = githubUser
+                password = githubPassword
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("mavenJava") {
+            pom {
+                name.set("su-meldinger")
+                description.set("Inneholder funksjonalitet knyttet til meldinger som utveksles mellom su-applikasjoner")
+                url.set("https://github.com/navikt/su-meldinger")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/navikt/su-meldinger.git")
+                    developerConnection.set("scm:git:https://github.com/navikt/su-meldinger.git")
+                    url.set("https://github.com/navikt/su-meldinger")
+                }
+            }
+            from(components["java"])
+        }
+    }
+}
+
 val junitJupiterVersion = "5.6.0-M1"
 val orgJsonVersion = "20180813"
 
 dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("ch.qos.logback:logback-classic:1.2.3")
     implementation("net.logstash.logback:logstash-logback-encoder:5.2")
     implementation("org.json:json:$orgJsonVersion")
